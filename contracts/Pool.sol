2d594112121102f7d9a687a05b9d036cd2e9deab
Benmissi-A <aminebenmissi@hotmail.fr>
2021-08-11 21:47:50 +0200

    pool in progress

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -3,13 +3,25 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Pool {
-     address private _poolOwner;
-     uint256 private _initialBalance;
+contract Pool is IERC20{
+    bool private _init;
+ 
+     address private _assetA;
+     address private _assetB;
+     
+     uint256 private _balanceA;
+     uint256 private _balanceB;
 
-    constructor( address poolOwner_ , uint256 initialBalance_){
-        _poolOwner = poolOwner_ ;
-        _initialBalance = initialBalance_ ;
+    constructor(_assetA , _assetB){}
+
+    }
+    
+    
+    
+    function deposit ()  {
+        _init = true;
+        _poolOwner = address(msg.sender)
     }
 }
